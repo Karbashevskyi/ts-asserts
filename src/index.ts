e3683fac2f86e5e33adb6664a6eaff40329e7ed2
karba <ivankarbashevskyi@gmail.com>
2022-05-07 23:08:09 +0200

    Init methods for assert some data

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,44 @@
-export {LocalStorage} from './local-storage';
+import { Is } from "ts-checkers";
+
+export class Asserts {
+
+    /**
+     *
+     * @param input
+     */
+    public static assertString(input: any): asserts input is string {
+        if (typeof input !== 'string') {
+            throw new Error('Input must be a string!');
+        }
+    }
+
+    /**
+     *
+     * @param input
+     */
+    public static assertNumber(input: any): asserts input is number {
+        if (typeof input !== 'number') {
+            throw new Error('Input must be a number!');
+        }
+    }
+
+    /**
+     *
+     * @param input
+     */
+    public static assertBoolean(input: any): asserts input is boolean {
+        if (typeof input !== 'boolean') {
+            throw new Error('Input must be a boolean!');
+        }
+    }
+
+    /**
+     *
+     * @param input
+     */
+    public static assertNotNullOrUndefined(input: any) {
+        if (Is.nullOrUndefined(input)) {
+            throw new Error('Input must be not null or undefined!');
+        }
+    }
+}
