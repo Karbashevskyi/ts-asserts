3575a7fc4e48c2d304760ca504ee777b68ec6474
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2022-05-29 17:58:17 +0200

    fix

--- src/asserts/symbol.assert.ts
+++ src/asserts/symbol.assert.ts
@@ -4,7 +4,7 @@ import { Is } from 'ts-checkers';
  *
  * @param input type is any
  */
-export function SymbolAssert(input: any): asserts input is Symbol {
+export function SymbolAssert(input: any): asserts input is symbol {
   if (Is.Not.Symbol(input)) {
     throw new TypeError('Type of argument is not correct.');
   }
