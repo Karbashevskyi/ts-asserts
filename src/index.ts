792236d54e2e481d8954de93642717c68ec7d1a5
Ivan Karbashevskyi <ivan.karbashevskyi@gmail.com>
2022-05-29 11:58:21 +0200

    1.0.0

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,19 @@
-import {ArrayAssert} from './asserts/array.assert';
-import {BooleanAssert} from './asserts/boolean.assert';
-import {NotNullOrUndefinedAssert} from './asserts/not-null-or-undefined.assert';
-import {NumberAssert} from './asserts/number.assert';
-import {ObjectAssert} from './asserts/object.assert';
-import {StringAssert} from './asserts/string.assert';
-import {SymbolAssert} from './asserts/symbol.assert';
-import {BigIntAssert} from './asserts/bigint.assert';
+import { ArrayAssert } from './asserts/array.assert';
+import { BooleanAssert } from './asserts/boolean.assert';
+import { NotNullOrUndefinedAssert } from './asserts/not-null-or-undefined.assert';
+import { NumberAssert } from './asserts/number.assert';
+import { ObjectAssert } from './asserts/object.assert';
+import { StringAssert } from './asserts/string.assert';
+import { SymbolAssert } from './asserts/symbol.assert';
+import { BigIntAssert } from './asserts/bigint.assert';
 
 export const Asserts = {
-    Array: ArrayAssert,
-    BigInt: BigIntAssert,
-    Boolean: BooleanAssert,
-    NotNullOrUndefined: NotNullOrUndefinedAssert,
-    Number: NumberAssert,
-    Object: ObjectAssert,
-    String: StringAssert,
-    Symbol: SymbolAssert,
+  Array: ArrayAssert,
+  BigInt: BigIntAssert,
+  Boolean: BooleanAssert,
+  NotNullOrUndefined: NotNullOrUndefinedAssert,
+  Number: NumberAssert,
+  Object: ObjectAssert,
+  String: StringAssert,
+  Symbol: SymbolAssert,
 };
